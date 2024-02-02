5dbce13c42102913478bd23fd193d924611da6ec
hebertsanto <hebertsantos0704@gmail.com>
2024-02-02 16:21:41 -0300

    feat: create post

---
+++ src/domain/useCases/post/createPostUseCase.ts
@@ -0,0 +1,17 @@
+import { CreatePostRepository } from '../../../adapters/repositories/post/createPostRepository'
+
+export class CreatePostUseCase {
+  private createPost: CreatePostRepository
+
+  constructor() {
+    this.createPost = new CreatePostRepository()
+  }
+  async execute(title: string, content: string, userId: number) {
+    const createPost = await this.createPost.execute({
+      title,
+      content,
+      userId,
+    })
+    return createPost
+  }
+}
--- src/domain/useCases/post/deletePostUseCase.ts
+++ src/domain/useCases/post/deletePostUseCase.ts
@@ -1,18 +1,17 @@
-import { DeletePostRepository } from "../../../adapters/repositories/post/deletePostRepository";
+import { DeletePostRepository } from '../../../adapters/repositories/post/deletePostRepository'
 
 export class DeletePostUseCase {
-   private delete : DeletePostRepository;
+  private delete: DeletePostRepository
 
-   constructor(){
-    this.delete = new DeletePostRepository();
-   }
+  constructor() {
+    this.delete = new DeletePostRepository()
+  }
 
-   async execute(id : number){
-    
-     const post = await this.delete.execute(id);
+  async execute(id: number) {
+    const post = await this.delete.execute(id)
 
-     if(!post){
-        throw new Error('this post not exist');
-     }
-   }
+    if (!post) {
+      throw new Error('this post not exist')
+    }
+  }
 }
