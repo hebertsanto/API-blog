458c13067a9145044228b999836e88db88d1228a
hebertsanto <hebertsantos0704@gmail.com>
2024-02-02 16:48:17 -0300

    style: pretier improving commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'test']],
-        'subject-case': [2, 'always', 'sentence-case'],
-      },
-  };
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'test'],
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+}
