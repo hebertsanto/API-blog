d18fd8aac6e64608bef04c571a9dc8a829af1279
hebertsanto <hebertsantos0704@gmail.com>
2024-02-02 21:33:13 -0300

    feat: implement password hash

--- src/domain/useCases/user/createUserUseCase.ts
+++ src/domain/useCases/user/createUserUseCase.ts
@@ -1,5 +1,6 @@
 import { CreateUserRepository } from '../../../adapters/repositories/user/createUserRepository';
 import { IUser } from '../../../utils/@types';
+import bcrypt from 'bcrypt';
 
 export class UserUseCase {
   private userRepository: CreateUserRepository;
@@ -9,15 +10,24 @@ export class UserUseCase {
   }
 
   async create({ name, password, email }: IUser) {
+    const passwordhash = await this.hash(password);
     const newUser = await this.userRepository.execute({
       name,
       email,
-      password,
+      password : String(passwordhash),
     });
     return newUser;
   }
   async findUserExist(user: string) {
     const existerUser = await this.userRepository.findEmail(user);
     return existerUser;
+  }
+  async hash(password: string) {
+    try {
+      const hash = await bcrypt.hash(password, 10);
+      return hash;
+    } catch (error) {
+      return error;
+    }
   }
 }
