f9466f047b5bd86165fa0aa53890772580480081
hebertsanto <hebertsantos0704@gmail.com>
2024-02-06 13:10:55 -0300

    chore: add verify more simple

--- src/http/controllers/comment/get-controller.ts
+++ src/http/controllers/comment/get-controller.ts
@@ -1,35 +1,25 @@
 import { Request, Response } from 'express';
 import { makeGetCommentByIdUseCase } from '../../../use-cases/factories/comment/make-get-comment-use-case';
-import { prisma } from '../../../adapters/database/prismaClient';
-import { GenericsErros } from '../../../utils/errors/index.';
-
+import { GenericError } from '../../../utils/errors/index.';
 
 export const getCommentById = async (req: Request, res: Response) => {
-
   const { id } = req.params;
   const makeGetCommentById = await makeGetCommentByIdUseCase();
 
   try {
-
-    const commentExist = await prisma.comment.findUnique({
-      where: {
-        id: id
-      }
-    });
-
-    if (!commentExist) {
-      throw new GenericsErros('comment');
-    }
     const comment = await makeGetCommentById.execute(id);
 
+    if (!comment) {
+      throw new GenericError('comment');
+    }
     return res.status(200).json({
       msg: 'comment here',
       comment,
     });
   } catch (error) {
-    if (error instanceof GenericsErros) {
+    if (error instanceof GenericError) {
       res.status(400).json({
-        msg: 'esse id não existe'
+        msg: 'esse id não existe',
       });
     }
   }
