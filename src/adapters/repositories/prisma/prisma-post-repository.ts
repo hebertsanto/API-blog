26e1fb06768598e9fa3ea93c09bf9165ac2e74de
hebertsanto <hebertsantos0704@gmail.com>
2024-02-17 22:53:36 -0300

    chore: rename files

--- src/adapters/repositories/prisma/prisma-post-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Post, Prisma } from '@prisma/client';
-
-export interface PrismaPostRepository{
-    create(data: Prisma.PostUncheckedCreateInput): Promise<Post>
-    findById(id: string): Promise<Post | null>
-    findByIdAndDelete(id: string): Promise<Post | null>
-    findByIdAndUpdate(id: string): Promise<Post>
-}
