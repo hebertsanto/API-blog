4af2beb7460fda87ac910ebe9afd70b9beede8b0
hebertsanto <hebertsantos0704@gmail.com>
2024-02-18 21:12:31 -0300

    feat: express to get zod errors

--- src/infra/express/express.ts
+++ src/infra/express/express.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { config } from 'dotenv';
 config();
 import { router } from '../../routes';
+import { Request, Response } from 'express';
+import { ZodError } from 'zod';
 
 export class ExpressApp {
   static start() {
@@ -9,6 +11,16 @@ export class ExpressApp {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(router);
+
+    //eslint-disable-next-line
+    app.use((error : any, req: Request, res: Response) => {
+      if (error instanceof ZodError) {
+        return res.status(400).json({
+          msg: 'error validation data',
+          error
+        });
+      }
+    });
 
     app.listen(process.env.PORT, () => {
       console.log('server is running');
